7a83c485561020b5709ee35dcdc5344a5d21aa16
Dungasaur <dungasaurusrex@gmail.com>
2020-02-19 16:42:14 -0700

    dd

--- Assets/Scripts/Roomba/RoombaController.cs
+++ Assets/Scripts/Roomba/RoombaController.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class RoombaController : MonoBehaviour
 {
-	public float speed, rotationSpeed;
-	private bool rotating;
+	public enum State
+	{
+		Rotating,
+		Moving,
+		Following
+	};
+	public State state;
+
+	public float speed, rotationSpeed, dirtCheckRadius;
+	private Collider bumperCollider;
+	private bool coroutineIsRunning;
 	private Rigidbody rb;
-	public Collider bumperCollider;
-	
+	private LayerMask dirtLayer;
+	private GameObject currentTarget;
+
 	void Awake()
 	{
+		coroutineIsRunning = false;
+		dirtLayer = LayerMask.GetMask("Dirt");
 		rb = GetComponent<Rigidbody>();
 		foreach(Collider c in GetComponentsInChildren<Collider>())
 		{
@@ -33,29 +45,85 @@ public class RoombaController : MonoBehaviour
 
 	private void FixedUpdate()
 	{
-		if(!rotating)
+
+		switch (state)
 		{
-			transform.Translate(Vector3.forward * speed*Time.deltaTime);
+			case State.Rotating:
+				if(!coroutineIsRunning)
+				{
+					transform.Rotate(Vector3.up * rotationSpeed);
+
+				}
+				break;
+			case State.Moving:
+				Collider[] nearbyDirt = Physics.OverlapSphere(transform.position, dirtCheckRadius, dirtLayer, QueryTriggerInteraction.Collide);
+				if (nearbyDirt.Length > 1)
+				{
+					GameObject target = nearbyDirt[0].gameObject;
+					float distance = Vector3.Distance(transform.position, target.transform.position);
+					for (int i = 1; i < nearbyDirt.Length; i++)
+					{
+						float tempDistance = Vector3.Distance(transform.position, nearbyDirt[i].transform.position);
+						if (tempDistance < distance)
+						{
+							distance = tempDistance;
+							target = nearbyDirt[i].gameObject;
+						}
+					}
+					if(currentTarget != target)
+					{
+						currentTarget = target;
+						StartCoroutine("RotateDynamic", currentTarget.transform.position);
+					}
+				}
+				else if (nearbyDirt.Length == 1)
+				{
+					if (currentTarget != nearbyDirt[0].gameObject)
+					{
+						currentTarget = nearbyDirt[0].gameObject;
+						StartCoroutine("RotateDynamic", currentTarget.transform.position);
+					}
+				}
+				else
+				{
+					currentTarget = null;
+				}
+
+				transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+				break;
+			case State.Following:
+				break;
+			default:
+				break;
 		}
-		else
-		{
-			transform.Rotate(Vector3.up * rotationSpeed);
-		}
+		
 	}
 
 	void OnCollisionEnter(Collision collision)
 	{
 		if(collision.GetContact(0).thisCollider==bumperCollider)
 		{// Checking if hit collider is the front bumper. if it is, start rotating.
-			rotating = true;
+			state = State.Rotating;
 			StartCoroutine("RotateFixed");
 		}
 	}
 
 	IEnumerator RotateFixed()
 	{
+		currentTarget = null;
 		yield return new WaitForSeconds(1);
-		rotating = false;
+		
+		state = State.Moving;
+	}
+
+	IEnumerator RotateDynamic(Vector3 position)
+	{
+		coroutineIsRunning = true;
+		yield return null;
+		//Vector3.RotateTowards()
+		coroutineIsRunning = false;
+
 	}
 
 }
