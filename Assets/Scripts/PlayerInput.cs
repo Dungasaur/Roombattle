05dc6301c447270591ef8c946428c48a32951d9f
Dungasaur <47060952+Dungasaur@users.noreply.github.com>
2020-03-03 13:17:43 -0700

    started input

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	// Strings for Input
+	public string fire1, fire2, fire3, horizontal, vertical, submit, cancel;
+	float hValue, vValue;
+	private void Update()
+	{
+		if(Input.GetButton(submit))
+		{
+			//do submit
+		}
+		if(Input.GetButton(cancel))
+		{
+			//do cancel
+		}
+		if(Input.GetButton(fire1))
+		{
+			Fire1();
+		}
+		if(Input.GetButton(fire2))
+		{
+			Fire2();
+		}
+		if(Input.GetButtonDown(fire3))
+		{
+			Fire3();
+		}
+		
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void Fire1()
+	{
+
+	}
+
+	void Fire2()
+	{
+
+	}
+
+	void Fire3()
+	{
+
+	}
 }
