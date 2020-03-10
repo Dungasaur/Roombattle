86e75f8fe9ab3fa2f8def87b4bf83a4eaddb2065
Dungasaur <47060952+Dungasaur@users.noreply.github.com>
2020-03-10 14:52:33 -0600

    asdf

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -7,6 +7,14 @@ public class PlayerInput : MonoBehaviour
 	// Strings for Input
 	public string fire1, fire2, fire3, horizontal, vertical, submit, cancel;
 	float hValue, vValue;
+	public float speed;
+	private float relativeSpeed;
+	private RectTransform rectTransform;
+
+	private void Awake()
+	{
+		rectTransform = GetComponent<RectTransform>();
+	}
 	private void Update()
 	{
 		if(Input.GetButton(submit))
@@ -29,9 +37,13 @@ public class PlayerInput : MonoBehaviour
 		{
 			Fire3();
 		}
-		
+
+		hValue = Input.GetAxis(horizontal);
+		vValue = Input.GetAxis(vertical);
 	}
 
+
+	
 	void Fire1()
 	{
 
@@ -45,5 +57,30 @@ public class PlayerInput : MonoBehaviour
 	void Fire3()
 	{
 
+	}
+
+	private void FixedUpdate()
+	{
+
+		//var y = Screen.Height - Screen.Height / 2;      // 50% 
+		//var y = Screen.Height - Screen.Height / 10;    // 10% 
+		//var y = Screen.Height - Screen.Height / 5;      // 20% 
+		//var y = Screen.Height - Screen.Height / 3;      // 33% 
+
+		// Set a speed relative to a 1920x1080 screen. Get screen size and area. howmuch bigger the screen is than 100x100, multiply speed by that ratio to get the conistent speed.
+
+		Vector2 position = rectTransform.anchoredPosition;
+
+		rectTransform.Translate(new Vector3(hValue * speed, vValue * speed, 0));
+		Debug.Log(rectTransform.position);
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		speed /= 3f;
+	}
+	private void OnCollisionExit2D(Collision2D collision)
+	{
+		speed *= 3f;
 	}
 }
